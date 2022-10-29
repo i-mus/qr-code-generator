ad2c6b825c2d593382a20cf22acae2de82292e59
i-mus <mushthaqpullat@gmail.com>
2022-10-29 18:42:50 +0530

    first working version

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -1,6 +1,21 @@
 from tkinter import *
+import pyqrcode
+from PIL import ImageTk, Image
 
 root = Tk()
+
+
+def generate():
+    link_name = name_entry.get()
+    link = link_entry.get()
+    file_name = link_name + ".png"
+    url = pyqrcode.create(link)
+    url.png(file_name, scale=8)
+    image = ImageTk.PhotoImage(Image.open(file_name))
+    image_label = Label(image=image)
+    image_label.image = image
+    canvas.create_window(200, 450, window=image_label)
+
 
 canvas = Canvas(root, width=400, height=600)
 canvas.pack()
@@ -10,7 +25,7 @@ name_label = Label(root, text="Link Name:", fg="red", bg="cyan")
 link_label = Label(root, text="Link", fg="red", bg="cyan")
 name_entry = Entry(root)
 link_entry = Entry(root)
-generate_button = Button(root, text="Generate", bg="black", fg="white")
+generate_button = Button(root, text="Generate", bg="black", fg="white", command=generate)
 canvas.create_window(200, 100, window=name_label)
 canvas.create_window(200, 130, window=name_entry)
 canvas.create_window(200, 160, window=link_label)
